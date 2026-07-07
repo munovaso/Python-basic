fb3769bfad43f55059a7e7a5c4a94c57b47e8174
munovaso <moonse0k1218@gmail.com>
2026-07-07 16:01:18 +0900

    first python code

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+print ("hello world")
+
+# 애완동물을 소개해 주세요~
+animal = "고양이"
+name = "나비"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + "입니다.")
+'''print("우리집 " + animal + "의 이름은 " + name + "입니다.")'''
+
+print("나이는 " + str(age) + "살이고, " + hobby + "를 좋아합니다")
+print(name + "는 어른일까요? " + str(is_adult))
